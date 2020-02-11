69a81eebdb5d20c715619860ecca18e8a3c50122
Raul Ovalle Tiburcio <raulovallet@gmail.com>
2020-02-11 13:04:05 -0600

    [ADD] app pos_branch

---
+++ pos_branch/static/src/js/pos_extended.js
@@ -0,0 +1,29 @@
+
+//console.log("custom js caleedddddddddddddddddddddddddddddddddd")
+odoo.define('pos_branch.pos_extended', function (require) {
+	"use strict";
+
+	var models = require('point_of_sale.models');
+	var screens = require('point_of_sale.screens');
+	var core = require('web.core');
+	var gui = require('point_of_sale.gui');
+	var popups = require('point_of_sale.popups');
+	
+
+	var QWeb = core.qweb;
+	var _t = core._t;
+
+	var _super_posmodel = models.PosModel.prototype;
+	models.PosModel = models.PosModel.extend({
+		initialize: function (session, attributes) {
+			var session_model = _.find(this.models, function(model){ return model.model === 'pos.session'; });
+			session_model.fields.push('branch_id');
+			return _super_posmodel.initialize.call(this, session, attributes);
+		},
+
+	});
+
+
+	
+
+});
