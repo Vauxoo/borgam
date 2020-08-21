ffcd00f7829110c9b85276e6e4e12906a83ba670
Hugo Adan <hugo@vauxoo.com>
2020-08-21 17:11:06 -0500

    [REM] All exposed modules moved to gitlab/vauxoo/pasteleria repo in order standardise this repo

--- multi-company/models/product.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, models, fields, _
-
-class Product(models.Model):
-    _inherit = 'product.template'
-
-    restricted_company_id = fields.Many2one('res.company', string="Restricted Company")
